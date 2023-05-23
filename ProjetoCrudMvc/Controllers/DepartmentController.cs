45a65f7c9d688c254b33d7e379537db8d6d4bd33
evandrolucas60 <evandrolucas60@yahoo.com>
2023-05-23 16:29:27 -0300

    Creating a DepartmentController and a visualization of existing data by action/view Index

---
+++ ProjetoCrudMvc/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoCrudMvc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoCrudMvc.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IESContext _context;
+
+        public DepartmentController(IESContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departments.OrderBy(c => c.Name).ToListAsync());
+        }
+    }
+}
